fb16e216cc2855b26968aa1fba40f31f39705456
Zengyj <zengyj11111@gmail.com>
2023-10-19 17:15:26 +0800

    Switch to ForgeGradle 5 & official mapping and upgrade to gradle 7.5.1

--- src/main/kotlin/com/the9grounds/aeadditions/block/BlockCraftingStorage.kt
+++ src/main/kotlin/com/the9grounds/aeadditions/block/BlockCraftingStorage.kt
@@ -1,6 +1,11 @@
 package com.the9grounds.aeadditions.block
 
+import net.minecraft.tileentity.TileEntity
+import net.minecraft.world.World
 import appeng.block.crafting.BlockCraftingStorage as AppEngBlockCraftingStorage
 
 class BlockCraftingStorage(type: CraftingUnitType?) : AppEngBlockCraftingStorage(type) {
+    override fun createNewTileEntity(worldIn: World, meta: Int): TileEntity? {
+        TODO("Not yet implemented")
+    }
 }
