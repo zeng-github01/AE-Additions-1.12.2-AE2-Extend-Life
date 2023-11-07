8d32c2f01f90538880be4655f59e11d992baf305
Zengyj <zengyj11111@gmail.com>
2023-11-07 08:49:34 +0800

    Downgrade mappings to snapshot to avoid conflict

--- src/main/kotlin/com/the9grounds/aeadditions/block/BlockCraftingStorage.kt
+++ src/main/kotlin/com/the9grounds/aeadditions/block/BlockCraftingStorage.kt
@@ -1,12 +1,9 @@
 package com.the9grounds.aeadditions.block
 
-import com.the9grounds.aeadditions.tileentity.TileEntityCraftingStorage
+import net.minecraft.block.Block
 import net.minecraft.tileentity.TileEntity
 import net.minecraft.world.World
 import appeng.block.crafting.BlockCraftingStorage as AppEngBlockCraftingStorage
 
 class BlockCraftingStorage(type: CraftingUnitType?) : AppEngBlockCraftingStorage(type) {
-    override fun createNewTileEntity(worldIn: World, meta: Int): TileEntity? {
-        return TileEntityCraftingStorage()
-    }
 }
