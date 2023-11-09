c0c91a3b9ddb2e4944c829f78c8b525c948b8d8f
Zengyj <zengyj11111@gmail.com>
2023-11-09 08:47:30 +0800

    Use self-build dev ae2 jar to get unobfuscated file

--- src/main/kotlin/com/the9grounds/aeadditions/block/BlockCraftingStorage.kt
+++ src/main/kotlin/com/the9grounds/aeadditions/block/BlockCraftingStorage.kt
@@ -6,7 +6,4 @@ import net.minecraft.world.World
 import appeng.block.crafting.BlockCraftingStorage as AppEngBlockCraftingStorage
 
 class BlockCraftingStorage(type: CraftingUnitType?) : AppEngBlockCraftingStorage(type) {
-    override fun createNewTileEntity(worldIn: World, meta: Int): TileEntity? {
-        return TileEntityCraftingStorage()
-    }
 }
